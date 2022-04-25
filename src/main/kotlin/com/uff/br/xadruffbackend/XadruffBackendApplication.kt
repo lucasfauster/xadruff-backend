74b5890d9b254f9f1a38d65d2fa96a54ff370ed0
igorcruzf <igfigueiredo@uolinc.com>
2022-04-25 19:25:20 -0300

    Initial commit.

---
+++ src/main/kotlin/com/uff/br/xadruffbackend/XadruffBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.uff.br.xadruffbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XadruffBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<XadruffBackendApplication>(*args)
+}
