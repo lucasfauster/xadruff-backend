da4c46ed3854f9e4aeaf4af69aa1cb1caf5b1331
igorcruzf <igfigueiredo@uolinc.com>
2022-04-25 21:02:06 -0300

    UFF-022 - alterado o tabuleiro para ser uma matriz

--- src/main/kotlin/com/uff/br/xadruffbackend/ChessService.kt
+++ src/main/kotlin/com/uff/br/xadruffbackend/ChessService.kt
@@ -5,17 +5,16 @@ import com.uff.br.xadruffbackend.model.Board
 class ChessService {
 
     fun createInitialBoard(): Board {
-        val positions = mutableListOf(
-            "R", "N", "B", "Q", "K", "B", "N", "R",
-            "P", "P", "P", "P", "P", "P", "P", "P",
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            "p", "p", "p", "p", "p", "p", "p", "p",
-            "r", "n", "b", "q", "k", "b", "n", "r"
+        val positions = listOf(
+            mutableListOf("r", "n", "b", "q", "k", "b", "n", "r"),
+            mutableListOf("p", "p", "p", "p", "p", "p", "p", "p"),
+            mutableListOf("", "", "", "", "", "", "", ""),
+            mutableListOf("", "", "", "", "", "", "", ""),
+            mutableListOf("", "", "", "", "", "", "", ""),
+            mutableListOf("", "", "", "", "", "", "", ""),
+            mutableListOf("P", "P", "P", "P", "P", "P", "P", "P"),
+            mutableListOf("R", "N", "B", "Q", "K", "B", "N", "R")
         )
-
         return Board(positions = positions)
     }
 }
--- src/main/kotlin/com/uff/br/xadruffbackend/model/Board.kt
+++ src/main/kotlin/com/uff/br/xadruffbackend/model/Board.kt
@@ -1,5 +1,5 @@
 package com.uff.br.xadruffbackend.model
 
-data class Board(val positions: MutableList<String?>)
+data class Board(val positions: List<MutableList<String>>)
 
 
