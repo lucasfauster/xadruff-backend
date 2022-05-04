62f1223f38e63eb7bc4f909a00b8378bf53f79c7
fausterlucas <lfauster@uolinc.com>
2022-05-04 16:38:46 -0300

    UFF-029 organizando estrutura do projeto e refatorando código aplicando boas práticas

--- src/test/kotlin/com/uff/br/xadruffbackend/MinimaxTest.kt
+++ src/test/kotlin/com/uff/br/xadruffbackend/MinimaxTest.kt
@@ -1,13 +1,13 @@
 package com.uff.br.xadruffbackend
 
-import com.uff.br.xadruffbackend.model.BoardMock
-import com.uff.br.xadruffbackend.model.Minimax
+import com.uff.br.xadruffbackend.AI.AIService
+import com.uff.br.xadruffbackend.AI.model.BoardMock
 import org.junit.jupiter.api.Test
 
 class MinimaxTest {
 
     private val boardMock = BoardMock()
-    private val minimax = Minimax()
+    private val minimax = AIService()
 
     @Test
     fun `should return a move`(){
