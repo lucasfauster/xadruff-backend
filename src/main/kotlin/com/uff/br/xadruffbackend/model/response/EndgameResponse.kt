6669b2cebc2855552372e4ff374b0052b90e3411
igorcruzf <igfigueiredo@uolinc.com>
2022-06-17 23:02:49 -0300

    UFF-025 - Ajustando formato json da response

--- src/main/kotlin/com/uff/br/xadruffbackend/model/response/EndgameResponse.kt
+++ src/main/kotlin/com/uff/br/xadruffbackend/model/response/EndgameResponse.kt
@@ -1,6 +1,10 @@
 package com.uff.br.xadruffbackend.model.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 class EndgameResponse(
     val winner: String,
+
+    @JsonProperty("endgame_message")
     val endgameMessage: String
 )
